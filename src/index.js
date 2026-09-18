bee31a60f9b13a7d858d71e51c49e946e2b89e09
Aman <amancreator25@gmail.com>
2026-09-18 19:36:22 +0530

    refactor: streamline server startup by removing dotenv configuration and adjusting listen parameters

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import dotenv from "dotenv";
-dotenv.config();
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
-
-
 const PORT = process.env.PORT || 8000;
 
-
-
-
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
